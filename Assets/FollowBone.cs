227f90fe3caf3c33aa69d6f9882ed12cdca7bfd8
Unknown <qq265@nyu.edu>
2017-10-30 16:54:49 -0400

    Basic test

---
+++ Assets/FollowBone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBone : MonoBehaviour {
+	
+	public float speed = 1.0f;
+	public bool pickedUp = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		followBone ();
+	}
+
+	public void IsPickedUp(){
+		pickedUp = true;
+	}
+
+	public void IsDetached(){
+		pickedUp = false;
+	}
+	public void followBone(){
+		//transform.position = Vector3.MoveTowards (transform.position,new Vector3(0,1,0), speed*Time.deltaTime);
+		if(pickedUp == true){
+			transform.position = Vector3.Lerp(transform.position, new Vector3(-1.86f, 0.7f, -0.28f), speed * Time.deltaTime);
+		}
+
+	}
+}
