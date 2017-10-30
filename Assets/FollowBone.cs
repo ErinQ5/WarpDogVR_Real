e7d2992e1848d95da0988b4a5842a9c2c11ecf2d
JonnyLoo <Jonliu44@gmail.com>
2017-10-30 17:29:26 -0400

    dog follows bone

--- Assets/FollowBone.cs
+++ Assets/FollowBone.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowBone : MonoBehaviour {
-	
-	public float speed = 1.0f;
-	public bool pickedUp = false;
+
+    bool pickedUp = false;
+    bool isColliding = false;
+
+    Vector3 noYlocation;
+    public float speed = 1.0f;
+    public Transform bone;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        noYlocation = new Vector3(bone.transform.position.x, .55f, bone.transform.position.z);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		followBone ();
+        noYlocation = new Vector3(bone.transform.position.x, .55f, bone.transform.position.z);
+        followBone ();
 	}
 
 	public void IsPickedUp(){
@@ -23,11 +29,15 @@ public class FollowBone : MonoBehaviour {
 	public void IsDetached(){
 		pickedUp = false;
 	}
+
 	public void followBone(){
 		//transform.position = Vector3.MoveTowards (transform.position,new Vector3(0,1,0), speed*Time.deltaTime);
-		if(pickedUp == true){
-			transform.position = Vector3.Lerp(transform.position, new Vector3(-1.86f, 0.7f, -0.28f), speed * Time.deltaTime);
+		if(pickedUp == true && isColliding == false){
+			transform.position = Vector3.Lerp(transform.position, noYlocation, speed * Time.deltaTime);
 		}
-
 	}
+
+    public void OnCollisionStay(Collision collision) {
+        isColliding = true;
+    }
 }
