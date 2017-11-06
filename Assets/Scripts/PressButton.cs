8ea96ff3922bd66f410596c84a97ec7cd9f94bf3
JonnyLoo <Jonliu44@gmail.com>
2017-11-06 17:26:32 -0500

    puzzle 1 update

--- Assets/Scripts/PressButton.cs
+++ Assets/Scripts/PressButton.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -33,37 +32,3 @@ public class PressButton : MonoBehaviour {
         }
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Valve.VR.InteractionSystem;
-
-public class PressButton : MonoBehaviour {
-
-	public int value;
-	public Transform t;
-	PuzzleOneCode buttons;
-	Vector3 originalposition;
-
-	void Start() {
-		buttons = t.GetComponent<PuzzleOneCode>();
-		originalposition = this.transform.position;
-	}
-
-	void HandHoverUpdate(Hand hand) {
-		if (hand.GetStandardInteractionButtonDown()) {
-			this.transform.position = Vector3.Lerp(this.transform.position, this.transform.position + Vector3.down, Time.deltaTime * 5f);
-			if (value == 0) {
-				buttons.checkMatch();
-			}
-			else {
-				buttons.enterCode(value);
-			}
-		}
-		else {
-			this.transform.position = originalposition;
-		}
-	}
-}
->>>>>>> c91d12e07fe11886728596a676679740c6e86789
