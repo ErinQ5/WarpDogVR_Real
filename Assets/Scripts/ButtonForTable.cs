b2c92bfa7079a1ac9aca2688cf74f63c91babb7e
Unknown <qq265@nyu.edu>
2017-11-26 18:12:37 -0500

    Big environmental changes in the scene!!
    Windows, Space skybox, darker lights, more soptlights,  Start scene to adjust player's facing direction etc

--- Assets/Scripts/ButtonForTable.cs
+++ Assets/Scripts/ButtonForTable.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
+using UnityEngine.SceneManagement;
 
 public class ButtonForTable : MonoBehaviour {
 
     void HandHoverUpdate(Hand hand) {
         if (hand.GetStandardInteractionButtonDown()) {
-
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
         }
         else {
 
