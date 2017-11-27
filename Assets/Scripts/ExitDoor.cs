dfe39f5d397903e74b9837a4213f2e459b2d89a3
JonnyLoo <Jonliu44@gmail.com>
2017-11-26 22:18:10 -0500

    rachety version of exit

---
+++ Assets/Scripts/ExitDoor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ExitDoor : MonoBehaviour {
+
+    public Transform door;
+    Vector3 originalposition;
+
+    void Start() {
+        originalposition = this.transform.position;
+    }
+
+    void HandHoverUpdate(Hand hand) {
+        if (hand.GetStandardInteractionButtonDown()) {
+            this.transform.position = Vector3.Lerp(this.transform.position, this.transform.position + Vector3.right, Time.deltaTime * 5f);
+            door.transform.position = Vector3.Lerp(door.transform.position, door.transform.position + 90f * Vector3.up, Time.deltaTime * 5f);
+        }
+        else {
+            this.transform.position = originalposition;
+        }
+    }
+}
