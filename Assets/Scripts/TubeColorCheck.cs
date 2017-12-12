e8f11fdb306b8d8dc5ba94d076b0aff825afb350
Unknown <qq265@nyu.edu>
2017-12-12 17:09:20 -0500

    TestTube connections color change fixed, fence all removable

---
+++ Assets/Scripts/TubeColorCheck.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TubeColorCheck : MonoBehaviour {
+	public TestTubeManager testtubeManager;
+	public Renderer tube_1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tube_1.material.SetColor ("_EmissionColor", testtubeManager.myColor_1);
+	}
+}
