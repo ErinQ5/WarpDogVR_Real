70abb38e031815b907d0b4da36185dd8f83b94dc
Jon Liu <jonliu44@gmail.com>
2017-12-12 23:07:14 -0500

    Revert "added raycast"

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -1,41 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Raycast : MonoBehaviour {
 
-    // Use this for initialization
-    void Start() {
-
-    }
-
-    public Image progressImage;
-    Transform lastThingWeLookedAt;
-    float timeLookedAt = 0f;
-
-    void Update() {
-
-        Ray myRay = new Ray(transform.position, transform.forward);
-        float myRaycastDistance = 10f;
-        Debug.DrawRay(myRay.origin, myRay.direction * myRaycastDistance, Color.magenta);
-        RaycastHit myRaycastHit = new RaycastHit();
-        if (Physics.Raycast(myRay, out myRaycastHit, myRaycastDistance)) {
-            lastThingWeLookedAt = myRaycastHit.transform;
-            Debug.Log("raycast hit " + lastThingWeLookedAt.name);
-
-            if (lastThingWeLookedAt.name == "press") {
-                timeLookedAt += Time.deltaTime;
-                if (timeLookedAt >= 2f) {
-                    lastThingWeLookedAt.transform.localScale *= 1.1f;
-                    timeLookedAt = 0f;
-                }
-            }
-        }
-        else {
-            timeLookedAt = Mathf.Clamp(timeLookedAt - Time.deltaTime, 0f, 1f);
-        }
-
-        progressImage.fillAmount = timeLookedAt/2;
-    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
